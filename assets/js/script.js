c7f2ffd43080676e07a946b668e944ba155bb63c
Paolo Galli <paologalli@Mini-di-Paolo.station>
2023-10-24 20:13:57 +0200

    third commit
    added contact form and script for btnReset

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,15 @@
+"use strict";
+
 // Reveal section
 const allSection = document.querySelectorAll(".section");
+
+const btnSubmit = document.querySelector(".btn__submit");
+const btnReset = document.querySelector(".btn__reset");
+
+const inputFirstName = document.querySelector(".form__input-firstname");
+const inputLastName = document.querySelector(".form__input-lastname");
+const inputEmail = document.querySelector(".form__input-email");
+const inputMessage = document.querySelector(".form__input-message");
 
 const revealSection = function (entries, observer) {
   const [entry] = entries;
@@ -16,4 +26,13 @@ const obsSection = new IntersectionObserver(revealSection, {
 allSection.forEach(function (section) {
   obsSection.observe(section);
   section.classList.add("section--hidden");
+});
+
+btnReset.addEventListener("click", function (e) {
+  inputFirstName.value =
+    inputLastName.value =
+    inputEmail.value =
+    inputMessage.value =
+      "";
+  inputFirstName.focus();
 });

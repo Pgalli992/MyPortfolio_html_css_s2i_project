aebd44a4277b15d1b0ef6ed7c54be54e691fbe3b
Paolo Galli <paologalli@Mini-di-Paolo.station>
2023-11-19 09:26:24 +0100

    fifth commit
    working with m.q. $bp-large

--- assets/js/scriptIndex.js
+++ assets/js/scriptIndex.js
@@ -1,45 +1,6 @@
 "use strict";
 
-const navbar = document.querySelector(".navigation__nav");
-const allInterest = document.querySelectorAll(".section__interests-box");
-const interestCoding = document.querySelector(".section__interests_coding");
-const interestBlockchain = document.querySelector(
-  ".section__interests_blockchain"
-);
-const interestCrypto = document.querySelector(".section__interests_crypto");
-
-// Buttons
-const btnContactMe = document.querySelector(".header__main--nav-cta");
-const btnSubmit = document.querySelector(".btn__submit");
-const btnReset = document.querySelector(".btn__reset");
-
-// Input fields
-const inputFirstName = document.querySelector(".form__input-firstname");
-const inputLastName = document.querySelector(".form__input-lastname");
-const inputEmail = document.querySelector(".form__input-email");
-const inputMessage = document.querySelector(".form__input-message");
-
-// Sections
-const header = document.querySelector(".header");
-const sectionInterests = document.querySelector(".section__interests");
-
-// Page scrolling from navbar
-navbar.addEventListener("click", function (e) {
-  e.preventDefault();
-  if (e.target.classList.contains("navigation__link")) {
-    const id = e.target.getAttribute("href");
-    document.querySelector(id).scrollIntoView({ behavior: "smooth" });
-  }
-});
-// Page scrolling from "Contact me" header button
-btnContactMe.addEventListener("click", function (e) {
-  e.preventDefault();
-  const id = e.target.getAttribute("href");
-  document.querySelector(id).scrollIntoView({ behavior: "smooth" });
-});
-
 // Functions
-
 const clearForm = function (e) {
   inputFirstName.value =
     inputLastName.value =
@@ -50,18 +11,51 @@ const clearForm = function (e) {
 };
 
 const sendMail = function (e) {
-  const variables = {
-    from_name: document.getElementById("firstname").value,
-    from_lastname: document.getElementById("lastname").value,
-    message: document.getElementById("message").value,
-    email: document.getElementById("email").value,
-  };
-  emailjs
-    .send("contact_service", "portfolio_contact_form", variables)
-    .then(function (res) {
-      alert("Success!");
-    });
+  if (
+    (inputFirstName.value !== "") &
+    (inputLastName.value !== "") &
+    (inputEmail.value !== "")
+  ) {
+    const variables = {
+      from_name: document.getElementById("firstname").value,
+      from_lastname: document.getElementById("lastname").value,
+      message: document.getElementById("message").value,
+      email: document.getElementById("email").value,
+    };
+    emailjs
+      .send("contact_service", "portfolio_contact_form", variables)
+      .then(function (res) {
+        alert("Success!");
+      });
+  } else {
+    alert("Please fill out the form");
+  }
 };
+
+// Page scrolling from navbar
+/*
+******************************************************************************************************************
+
+// It does not work from links that pointing different html page.
+navbar.addEventListener("click", function (e) {
+  e.preventDefault();
+  if (e.target.classList.contains("navigation__link")) {
+    const id = e.target.getAttribute("href");
+    document.querySelector(id).scrollIntoView({ behavior: "smooth" });
+  }
+});
+******************************************************************************************************************
+*/
+navLinkWhoIAm.addEventListener("click", scrollTo);
+navLinkInterests.addEventListener("click", scrollTo);
+
+// Page scrolling from "Contact me" header button
+btnContactMe.addEventListener("click", function (e) {
+  e.preventDefault();
+  const id = e.target.getAttribute("href");
+  document.querySelector(id).scrollIntoView({ behavior: "smooth" });
+});
+
 // Reveal section
 
 /*
@@ -106,32 +100,12 @@ allInterest.forEach(function (interest) {
   interestCrypto.classList.add("interest--hidden");
 });
 
-// Reset button functionn
+// Reset button function
 btnReset.addEventListener("click", clearForm);
 
-// Sticky navigation
-const obs = new IntersectionObserver(
-  function (entries) {
-    const ent = entries[0];
-    // console.log(ent);
-
-    if (!ent.isIntersecting) {
-      document.body.classList.add("sticky");
-    }
-
-    if (ent.isIntersecting) {
-      document.body.classList.remove("sticky");
-    }
-  },
-  {
-    // In the viewport
-    root: null,
-    threshold: 0,
-    rootMargin: "-80px",
-  }
-);
-obs.observe(header);
-
-// Event hanndler
+// Event handler
 btnSubmit.addEventListener("click", sendMail);
 btnSubmit.addEventListener("click", clearForm);
+
+// Observer
+obs.observe(header);
